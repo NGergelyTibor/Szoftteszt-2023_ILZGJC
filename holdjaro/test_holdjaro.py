b455fe6416ce3a35f33c59cac30853cc03b6b0f6
Nemesi Gergely Tibor <91251134+NGergelyTibor@users.noreply.github.com>
2023-10-17 17:22:02 +0200

    Test #6: Boundary test [FAIL]

--- holdjaro/test_holdjaro.py
+++ holdjaro/test_holdjaro.py
@@ -16,11 +16,18 @@ class TestHoldjaro(unittest.TestCase):
         holdjaro = Holdjaro(0, 0, 'N', [], 100)
         result = holdjaro.move('l')
         self.assertEqual(result, (0, 0, 'W'))
-        
+
     def test_turn_right(self):
         holdjaro = Holdjaro(0, 0, 'E', [], 100)
         result = holdjaro.move('r')
         self.assertEqual(result, (0, 0, 'S'))
+        
+    def test_boundary_handling(self):
+        obstacles = [(10, 10), (20, 30)]
+        planet_radius = 50
+        holdjaro = Holdjaro(45, 0, 'E', obstacles, planet_radius)
+        result = holdjaro.move('f')
+        self.assertEqual(result, (50, 0, 'E'))
 
 if __name__ == '__main__':
     unittest.main()
