309eb4787c38b942377e86467608eabe31a090d4
Nemesi Gergely Tibor <91251134+NGergelyTibor@users.noreply.github.com>
2023-10-17 17:10:48 +0200

    Test #2: Backwards test [fail]

--- holdjaro/test_holdjaro.py
+++ holdjaro/test_holdjaro.py
@@ -7,5 +7,10 @@ class TestHoldjaro(unittest.TestCase):
         result = holdjaro.move('f')
         self.assertEqual(result, (0, 1, 'N'))
 
+    def test_backward_movement(self):
+        holdjaro = Holdjaro(0, 0, 'E', [], 100)
+        result = holdjaro.move('b')
+        self.assertEqual(result, (1, 0, 'E'))
+
 if __name__ == '__main__':
     unittest.main()
